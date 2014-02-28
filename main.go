2a1de633c63612d9c6c719aee9ef98ad3b462ccd
Ben Cox <ben@benjojo.co.uk>
2014-02-28 23:30:45 +0000

    Added more code too support operation of time outs

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func main() {
-
+	if len(os.Args) == 0 {
+		log.Fatal("Please put a command after this.")
+	}
 	cmd := exec.Command(os.Args[0], os.Args[1:]...)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -15,5 +18,16 @@ func main() {
 	}
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func monitor(poke chan bool) {
+	for {
+		select {
+		case <-poke:
+			// a read from ch has occurred
+		case <-time.Sleep(time.Minute):
+			// the read from ch has timed out
+		}
 	}
 }
