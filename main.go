4dc2cbbd3943c11458f123e03170b811a99cad6c
Ben Cox <ben@benjojo.co.uk>
2014-02-28 23:26:05 +0000

    Added sample code, and plan on how to do this

--- main.go
+++ main.go
@@ -4,5 +4,29 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 )
+
+func main() {
+
+	cmd := exec.Command(os.Args[0], os.Args[1:]...)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	var person struct {
+		Name string
+		Age  int
+	}
+	if err := json.NewDecoder(stdout).Decode(&person); err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s is %d years old\n", person.Name, person.Age)
+}
