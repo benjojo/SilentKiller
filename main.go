58d8ee5986b4f13dd9da5cf7602413c1260c24b7
Ben Cox <ben@benjojo.co.uk>
2014-02-28 23:38:31 +0000

    Fixed cmd line sanity check.  output is redirected now. Done! :shipit:

--- main.go
+++ main.go
@@ -8,16 +8,27 @@ import (
 )
 
 func main() {
-	if len(os.Args) == 0 {
+	if len(os.Args) == 1 {
 		log.Fatal("Please put a command after this.")
 	}
-	cmd := exec.Command(os.Args[0], os.Args[1:]...)
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
 	}
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
+	}
+	buf := make([]byte, 1024)
+	pkc := make(chan bool)
+	go monitor(pkc)
+	for {
+		n, e := stdout.Read(buf)
+		if e != nil {
+			os.Exit(1)
+		}
+		os.Stdout.Write(buf[:n])
+		pkc <- true
 	}
 }
 
@@ -26,8 +37,10 @@ func monitor(poke chan bool) {
 		select {
 		case <-poke:
 			// a read from ch has occurred
-		case <-time.After(time.Minute):
+		case <-time.After(time.Second * 5):
 			// the read from ch has timed out
+			log.Fatal("Timed out")
+			os.Exit(0)
 		}
 	}
 }
