34213827764f366959c7136fa0ba9a7110284dfa
Ben Cox <ben@benjojo.co.uk>
2014-02-28 23:26:55 +0000

    Removed unused code

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -18,15 +16,4 @@ func main() {
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
 	}
-	var person struct {
-		Name string
-		Age  int
-	}
-	if err := json.NewDecoder(stdout).Decode(&person); err != nil {
-		log.Fatal(err)
-	}
-	if err := cmd.Wait(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s is %d years old\n", person.Name, person.Age)
 }
