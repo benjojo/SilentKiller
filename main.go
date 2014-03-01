10659a58445bf712f83ab460ce7a3b230a14cb24
Ben Cox <ben@benjojo.co.uk>
2014-03-01 13:53:14 +0000

    You can now tell it how long to wait before killing.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"strconv"
 	"time"
 )
 
@@ -13,7 +14,19 @@ func main() {
 		log.Fatal("Please put a command after this.")
 	}
 
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	var waittime int = 15
+	var cmd *exec.Cmd
+	if os.Args[1] == "-t" {
+		cmd = exec.Command(os.Args[3], os.Args[4:]...)
+		i, e := strconv.ParseInt(os.Args[2], 10, 64)
+		if e != nil {
+			log.Fatalf("Cannot turn %s into a number", os.Args[2])
+		}
+		waittime = int(i)
+	} else {
+		cmd = exec.Command(os.Args[1], os.Args[2:]...)
+	}
+
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
@@ -23,7 +36,7 @@ func main() {
 	}
 	buf := make([]byte, 1024)
 	pkc := make(chan bool)
-	go monitor(pkc, cmd)
+	go monitor(pkc, cmd, waittime)
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
@@ -44,12 +57,12 @@ func main() {
 	}
 }
 
-func monitor(poke chan bool, cmd *exec.Cmd) {
+func monitor(poke chan bool, cmd *exec.Cmd, waittime int) {
 	for {
 		select {
 		case <-poke:
 			// a read from ch has occurred
-		case <-time.After(time.Second * 5):
+		case <-time.After(time.Second * time.Duration(waittime)):
 			// the read from ch has timed out
 			log.Fatal("Timed out")
 			cmd.Process.Kill()
